23deda7d60a0581eeff5802f28be01ffe1ad0253
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-02-07 21:03:55 +0200

    added tag and task to model, make migrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    DONE_CHOICES = [(True, "Yes"), (False, "No"),]
+    content = models.TextField(max_length=500)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline_datetime = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(choices=DONE_CHOICES, default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self):
+        return (f"{self.tags} (content: {self.content}, "
+                f"deadline: {self.deadline_datetime}). Is done: {self.is_done}")
