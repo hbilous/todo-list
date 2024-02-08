9b4939296fa1f0a90cd8692574fd712de44f04f1
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-02-09 01:17:45 +0200

    added static styles.css, task and tag list, forms and buttons for create, update, delete, added all templates and includes empty sidebar

--- todo/models.py
+++ todo/models.py
@@ -9,13 +9,14 @@ class Tag(models.Model):
 
 
 class Task(models.Model):
-    DONE_CHOICES = [(True, "Yes"), (False, "No"),]
     content = models.TextField(max_length=500)
-    datetime = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     deadline_datetime = models.DateTimeField(null=True, blank=True)
-    is_done = models.BooleanField(choices=DONE_CHOICES, default=False)
+    is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
 
     def __str__(self):
-        return (f"{self.tags} (content: {self.content}, "
-                f"deadline: {self.deadline_datetime}). Is done: {self.is_done}")
+        tags_str = ", ".join(tag.name for tag in self.tags.all())
+        status = "Done" if self.is_done else "Not done"
+        return (f"{tags_str} (content: {self.content}, "
+                f"deadline: {self.deadline_datetime}). Is done: {status}")
--- todo_list_site/settings.py
+++ todo_list_site/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -117,9 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
